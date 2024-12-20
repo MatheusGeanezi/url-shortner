02451aa677019f852ab83ba6aa6978a4a74161c9
MatheusGeanezi <matheus.geanezi@yahoo.com.br>
2024-12-20 19:11:50 -0300

    chore: teste

--- config/database.ts
+++ config/database.ts
@@ -1,18 +1,18 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize("url_shortener", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+const sequelize = new Sequelize('url_shortener', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+})
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Conexão com o banco de dados bem-sucedida!");
+    console.log('Conexão com o banco de dados bem-sucedida!')
   })
   .catch((error) => {
-    console.error("Não foi possível conectar ao banco de dados:", error);
-  });
+    console.error('Não foi possível conectar ao banco de dados:', error)
+  })
 
-export default sequelize;
+export default sequelize
--- server.ts
+++ server.ts
@@ -1,30 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mysql from "mysql2";
-import dotenv from "dotenv";
-const app = express();
+import express from 'express'
+import bodyParser from 'body-parser'
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+const app = express()
 
 // import urlRoutes from "./routes/urlRoutes";
 
 // app.use("/api/url", urlRoutes);
 
-dotenv.config();
-app.use(bodyParser.json());
+dotenv.config()
+app.use(bodyParser.json())
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+  database: process.env.DB_NAME
+})
 
 connection.connect((err) => {
   if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err.message);
-    return;
+    console.error('Erro ao conectar ao banco de dados:', err.message)
+    return
   }
-  console.log("Conexão com o banco de dados estabelecida com sucesso!");
-});
-const PORT = 3000;
+  console.log('Conexão com o banco de dados estabelecida com sucesso!')
+})
+const PORT = 3000
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
